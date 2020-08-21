4bb34085085dfd514e23c52a98660bca62b75c6a
Osmond van Hemert <osmond@vanhemert.nu>
2020-08-21 10:08:40 +0200

    fix remote script

--- config/boot/start.sh
+++ config/boot/start.sh
@@ -3,7 +3,7 @@
 # execute your own script
 # echo "Put commands here."
 # or run a remote script
-# bash <(curl -s https://raw.githubusercontent.com/ovhemert/raspios-cloud-init/master/examples/change-hostname.sh)
+# curl -s https://raw.githubusercontent.com/ovhemert/raspios-cloud-init/master/examples/change-hostname.sh | bash -s
 
 # and clean exit
 exit 0
